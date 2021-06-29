948ab0313f7d7453e1dc148c6afcfc322ff997b0
shahaa <projects.salim95@gmail.com>
2021-06-29 15:36:14 +0500

    profile bounuses translate

--- .history/components_new/profile/Bonuses_20210629153339.tsx
+++ .history/components_new/profile/Bonuses_20210629153339.tsx
@@ -8,9 +8,11 @@ const Bonuses: FC = () => {
       <div className="text-2xl mt-8 mb-5">{tr('profile_bonuses_my')}</div>
       <div className="border flex h-28 justify-between p-10 rounded-2xl text-xl">
         <div className="font-bold">
-          {tr('profile_bounuses_current_balance')}
+          Текущий баланс {tr('profile_bounuses_current_balance')}
         </div>
-        <div className="text-yellow">0 {tr('profile_bounuses_score')} </div>
+        <div className="text-yellow">
+          0 ochkolar {tr('profile_bounuses_score')}{' '}
+        </div>
       </div>
     </div>
   )
