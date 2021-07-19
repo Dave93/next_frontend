cd9a1967ff95da52e20c3086a95939ab57323733
Davron Yuldashev <yul.davron.93@gmail.com>
2021-07-19 14:36:49 +0500

    ddd

--- components/product/helpers.ts
+++ components/product/helpers.ts
@@ -2,22 +2,6 @@ import type { Product, ProductOption } from '@commerce/types/product'
 export type SelectedOptions = Record<string, string | null>
 import { Dispatch, SetStateAction } from 'react'
 
-export function getProductVariant(product: Product, opts: SelectedOptions) {
-  const variant = product.variants?.find((variant) => {
-    return Object.entries(opts).every(([key, value]) =>
-      variant.options.find((option) => {
-        if (
-          option.__typename === 'MultipleChoiceOption' &&
-          option.displayName.toLowerCase() === key.toLowerCase()
-        ) {
-          return option.values.find((v) => v.label.toLowerCase() === value)
-        }
-      })
-    )
-  })
-  return variant
-}
-
 export function selectDefaultOptionFromProduct(
   product: Product,
   updater: Dispatch<SetStateAction<SelectedOptions>>
