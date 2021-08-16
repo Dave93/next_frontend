cd060d5c42ba35f04b4ac26b08229707630328d8
Davron Yuldashev <yul.davron.93@gmail.com>
2021-08-16 17:31:56 +0500

    news public

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -2,16 +2,50 @@ import { Layout } from '@components/common'
 import commerce from '@lib/api/commerce'
 import { GetServerSidePropsContext } from 'next'
 import NewsDetail from '@components_new/news/NewsDetail'
+import cookies from 'next-cookies'
+import axios from 'axios'
 
 export async function getServerSideProps({
   preview,
   locale,
   locales,
+  params,
+  ...context
 }: GetServerSidePropsContext) {
   const config = { locale, locales }
   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
   const { categories, brands, topMenu, footerInfoMenu, socials, cities } =
     await siteInfoPromise
+  const { id } = params
+  const c = cookies(context)
+
+  let activeCity = null
+
+  let activeCityData: any = c.activeCity
+  try {
+    activeCityData = Buffer.from(activeCityData, 'base64')
+    activeCityData = activeCityData.toString()
+    activeCityData = JSON.parse(activeCityData)
+  } catch (e) {}
+
+  activeCity = activeCityData
+
+  if (!activeCity) {
+    activeCity = cities[0]
+  }
+  console.log(activeCity)
+
+  const { data } = await axios.get(
+    `${process.env.API_URL}/api/news/public/${id}/?city_id=${activeCity.id}`
+  )
+
+  // console.log(data.data)
+
+  if (!data.data.length) {
+    return {
+      notFound: true,
+    }
+  }
 
   return {
     props: {
@@ -22,6 +56,7 @@ export async function getServerSideProps({
       socials,
       cleanBackground: true,
       cities,
+      newsItem: data.data[0],
     },
   }
 }
