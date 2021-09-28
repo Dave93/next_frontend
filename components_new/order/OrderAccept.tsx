717cbfe1f3ef86696f4994b5cd5ad9666dacacad
Doniman <doniman2409@gmail.com>
2021-09-28 13:50:01 +0500

    asdas

--- components_new/order/OrderAccept.tsx
+++ components_new/order/OrderAccept.tsx
@@ -162,7 +162,7 @@ const OrderAccept: FC<OrderDetailProps> = ({ order, orderStatuses }) => {
       </div>
       <div className="p-10 rounded-2xl text-xl mt-5 bg-white">
         <div className="text-lg mb-7 font-bold">{tr('delivery_address')}</div>
-        <div>{order?.billing_address}{order.house?  ", " + tr('house') + ": " + order.house : ""}{order.flat?  ", " + tr('flat') + ": " + order.flat : ""}{order.entrance?  ", " + tr('entrance') + ": " + order.entrance : ""}{order.door_code? ", " + tr('code_on_doors') + ": " + order.door_code : ""} {console.log(order)}</div>
+        <div>{order?.billing_address}{order.house?  ", " + tr('house') + ": " + order.house : ""}{order.flat?  ", " + tr('flat') + ": " + order.flat : ""}{order.entrance?  ", " + tr('entrance') + ": " + order.entrance : ""}{order.door_code? ", " + tr('code_on_doors') + ": " + order.door_code : ""}</div>
         <div></div>
       </div>
       <div className="p-10 rounded-2xl text-xl mt-5 bg-white">
