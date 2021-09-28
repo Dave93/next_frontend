13bb198f97d2dff3659017eb08afd686e88c770d
Davron Yuldashev <yul.davron.93@gmail.com>
2021-09-28 13:00:15 +0500

    dsad

--- components_new/order/Orders.tsx
+++ components_new/order/Orders.tsx
@@ -475,7 +475,9 @@ const Orders: FC<OrdersProps> = ({ channelName }: { channelName: any }) => {
   }
 
   const loadPickupItems = async () => {
-    const { data } = await axios.get(`${webAddress}/api/terminals/pickup`)
+    const { data } = await axios.get(
+      `${webAddress}/api/terminals/pickup?city_id=${activeCity.id}`
+    )
     let res: any[] = []
     data.data.map((item: any) => {
       if (item.latitude) {
@@ -2012,7 +2014,10 @@ const Orders: FC<OrdersProps> = ({ channelName }: { channelName: any }) => {
                               className="text-xs text-yellow mt-3 outline-none focus:outline-none border-b border-yellow pb-0.5"
                               onClick={(e) => getNewCode(e)}
                             >
-                              ${locale == 'uz' ? "Kodni qayta olish" : 'Получить код заново'}
+                              $
+                              {locale == 'uz'
+                                ? 'Kodni qayta olish'
+                                : 'Получить код заново'}
                             </button>
                           )}
                         </div>
@@ -2045,8 +2050,10 @@ const Orders: FC<OrdersProps> = ({ channelName }: { channelName: any }) => {
                                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                                 ></path>
                               </svg>
+                            ) : locale == 'uz' ? (
+                              'Tasdiqlash'
                             ) : (
-                              locale == 'uz' ? "Tasdiqlash" : 'Подтвердить'
+                              'Подтвердить'
                             )}
                           </button>
                         </div>
