23812ade5e9b16d07078001d571a8fc654691b1f
Doniman <doniman2409@gmail.com>
2021-10-04 17:51:08 +0500

    text doc

--- components_new/about/About.tsx
+++ components_new/about/About.tsx
@@ -1,11 +1,167 @@
 import useTranslation from 'next-translate/useTranslation'
 import { FC, memo } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 const About: FC = () => {
+  const { locale } = useRouter()
   const { t: tr } = useTranslation('common')
   return (
-    tr('about_text')
+    <>
+      {locale == 'ru' ? (
+        <div>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Бренд <strong>Chopar</strong> («гонец» с узбекского) - это часть
+              группы компаний{' '}
+              <strong>
+                <em>«</em>
+              </strong>
+              <strong>
+                Havoqand people<em>»</em>
+              </strong>
+              , успешно работает на рынке Узбекистана и СНГ.
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              С 2015 года бренд борется за звание лучшей сети пиццерий в
+              Республике и в данный момент оперирует более 19 филиалами по всей
+              стране и за ее пределами.&nbsp;
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              В 2021 году мы вышли на рынок Казахстана.&nbsp;
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <strong>
+              <span style={{ fontFamily: '"Times",serif' }}>Chopar</span>
+            </strong>
+            <span style={{ fontFamily: '"Times",serif' }}>
+              &nbsp;- это коллаборация восточных ценностей и современных
+              традиций.&nbsp;
+            </span>
+          </p>
+          <p>
+            <span style={{ fontSize: '16px', fontFamily: '"Times",serif' }}>
+              Семейная пицца Chopar - отличный повод провести время в компании
+              друзей или в семейном кругу. За 6 лет Chopar стал большой семьей,
+              командой, где ценятся таланты и вырастают успешные кадры. Нас
+              любят за качество и бережное отношение к традициям. Мы ценим
+              каждого нашего клиента, сохраняем стандарты и стремимся быть лучше
+              из года в год.
+            </span>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <strong>
+              <span style={{ fontFamily: '"Times",serif' }}>Chopar</span>
+            </strong>
+            <span style={{ fontFamily: '"Times",serif' }}>
+              &nbsp;brendi -{' '}
+              <strong>
+                <em>«</em>
+              </strong>
+              <strong>
+                Havoqand people<em>»</em>
+              </strong>{' '}
+              kompaniyalar guruhining bir qismi bo'lib, O'zbekiston va MDH
+              bozorida faoliyat yuritib kelmoqda.&nbsp;
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              2015-yildan buyon brendimiz O'zbekistondagi eng yaxshi
+              pitseriyalar tarmog'i maqomi uchun kurashmoqda va hozirgi paytda
+              mamlakatimizda va undan tashqarida __ dan ortiq filiallariga ega.
+              2021-yilda biz Qozog'iston bozoriga kirdik.&nbsp;
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <strong>
+              <span style={{ fontFamily: '"Times",serif' }}>Chopar</span>
+            </strong>
+            <span style={{ fontFamily: '"Times",serif' }}>
+              &nbsp;- sharqona qadriyatlar va zamonaviy an'analar
+              uyg'unligidir.&nbsp;
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Choparning oilaviy pitsasi - oila va do'stlar davrasida vaqt
+              o'tkazish uchun yaxshigina sabab.&nbsp;
+            </span>
+            <span style={{ fontFamily: '"Times",serif' }}>
+              6 yil ichida Chopar iqtidorlilar qadrlanadigan va muvaffaqiyatli
+              insonlar yetishib chiqadigan katta oilaga aylandi. Bizni sifat va
+              an'analarga bo'lgan nozik yondashuv uchun yaxshi ko'rishadi. Biz
+              har bir mijozimizni qadrlaymiz, standartlarni saqlagan holda
+              yildan-yilga yanada mukammal bo'lishga intilamiz
+            </span>
+          </p>
+        </div>
+      )}
+    </>
   )
 }
 
--- components_new/delivery/Delivery.tsx
+++ components_new/delivery/Delivery.tsx
@@ -1,11 +1,286 @@
 import useTranslation from 'next-translate/useTranslation'
 import { memo, FC } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
-const Delivery: FC = () => {
+const Delivery: FC = () => {  
+  const { locale } = useRouter()
   const { t: tr } = useTranslation('common')
   return (
-    tr("delivery_text")
+    <>
+      {locale == 'ru' ? (
+        <div>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Доставка ежедневно с 10:00 до 03:00.
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Телефон доставки +998971 205 11 11
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Оплата наличными, оплата картой, онлайн оплата
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Как сделать заказ в Chopar Pizza&nbsp;
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Достаточно просто выбрать заинтересовавшие вас блюда из меню и
+              добавить их в корзину (нажимайте на кнопку с ценой рядом с каждым
+              блюдом — оно будет добавлено в Ваш заказ), затем щёлкнуть по
+              изображению корзины в правом верхнем углу сайта и далее следовать
+              простым инструкциям по заполнению формы заказа.
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Зона А:&nbsp;При доставке по Ташкенту минимальная сумма заказа
+              составляет 60&nbsp;000 сум, среднее время доставки 40 минут
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Зона B: при доставке по ТКАД минимальная сумма заказа составляет
+              100 000 сум, среднее время доставки – 60 минут
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <strong>
+              <span style={{ fontFamily: '"Times",serif' }}>
+                Оплата наличными
+              </span>
+            </strong>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Для выбора оплаты товара наличными на странице заказа необходимо
+              выбрать пункт "Наличными при получении". Вы сможете оплатить товар
+              в нашем пункте самовывоза или при доставке курьером, оплата
+              осуществляется наличными курьеру в руки.
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <strong>
+              <span style={{ fontFamily: '"Times",serif' }}>Оплата картой</span>
+            </strong>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Для выбора оплаты товара картой при получении, на странице заказа
+              необходимо выбрать пункт "Картой при получении". Вы сможете
+              оплатить товар в нашем пункте самовывоза или при доставке
+              курьером, оплата курьеру осуществляется с помощью переносного
+              терминала оплаты.
+            </span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>&nbsp;</span>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <strong>
+              <span style={{ fontFamily: '"Times",serif' }}>Онлайн оплата</span>
+            </strong>
+          </p>
+          <p
+            style={{
+              margin: '0cm',
+              marginBottom: '.0001pt',
+              fontSize: '16px',
+              fontFamily: '"Calibri",sans-serif',
+            }}
+          >
+            <span style={{ fontFamily: '"Times",serif' }}>
+              Для выбора оплаты товара картой при получении, на странице заказа
+              необходимо выбрать пункт онлайн оплата Pay-me, Click. Вы сможете
+              оплатить товар в нашем пункте самовывоза или при доставке
+              курьером, оплата курьеру осуществляется с помощью переносного
+              терминала оплаты.
+            </span>
+          </p>
+        </div>
+      ) : (
+        <div>
+        <p>Yetkazib berish har kuni 10:00 dan 03:00 gacha.</p>
+        <p>Yetkazib berish telefoni +998 971 205 11 11</p>
+        <p>Naqd to'lov, karta orqali to'lash, onlayn to'lov</p>
+        <p><br /></p>
+        <p><br /></p>
+        <p>Chopar Pizza -ga qanday buyurtma berish mumkin</p>
+        <p>Siz menyudan o'zingizni qiziqtirgan taomlarni tanlab, ularni savatga qo'shishingiz kerak (har bir taom yonidagi narxi ko'rsatilgan tugmani bosing - bu sizning buyurtmangizga qo'shiladi), so'ngra savat tasvirini bosing. saytning o'ng yuqori burchagini bosing va keyin ariza to'ldirish bo'yicha oddiy ko'rsatmalarga amal qiling.</p>
+        <p><br /></p>
+        <p>A zonasi: Toshkentda etkazib berish uchun minimal buyurtma miqdori 60 000 so'm, etkazib berishning o'rtacha vaqti 40 minut</p>
+        <p><br /></p>
+        <p>B zonasi: TKAD orqali etkazib berish uchun minimal buyurtma miqdori 100 000 so'm, etkazib berishning o'rtacha vaqti 60 minut.</p>
+        <p><br /></p>
+        <p>Naqd to'lov</p>
+        <p>Buyurtma sahifasida tovarlar uchun naqd pul to'lashni tanlash uchun siz "Qabul qilingan pul" bandini tanlashingiz kerak. Siz tovarlarni bizning qabul qilish punktimizda yoki kurer orqali etkazib berishda to'lashingiz mumkin, to'lov qo'lda kurerga naqd pulda amalga oshiriladi.</p>
+        <p><br /></p>
+        <p>Karta orqali to'lov</p>
+        <p>Tovarlarni qabul qilishda karta orqali to'lovni tanlash uchun, buyurtma sahifasida "Qabul qilingan paytdagi karta" bandini tanlang. Siz tovarlarni bizning qabul qilish punktimizda yoki kurer orqali etkazib berishda to'lashingiz mumkin, kurerga to'lov ko'chma to'lov terminali yordamida amalga oshiriladi.</p>
+        <p><br /></p>
+        <p>Onlayn to'lov</p>
+        <p>Tovarlarni karta orqali qabul qilishda to'lovni tanlash uchun, buyurtma sahifasida siz "Pay-me, to'lov" onlayn-to'lovini tanlashingiz kerak. Siz tovarlarni bizning qabul qilish punktimizda yoki kurer orqali etkazib berishda to'lashingiz mumkin, kurerga to'lov ko'chma to'lov terminali yordamida amalga oshiriladi.</p>
+      </div>
+      )}
+    </>
   )
 }
 
