9c54d53ddcfa469984e69f56ff09778f13d79a59
Doniman <doniman2409@gmail.com>
2021-10-04 12:25:49 +0500

    добавил в меню ссылку присоединяйтесь к нам

--- components_new/header/HeaderMenu.tsx
+++ components_new/header/HeaderMenu.tsx
@@ -18,6 +18,9 @@ const HeaderMenu: FC<HeaderMenuItems> = ({ menuItems }) => {
             </li>
           )
         })}
+        <li className="px-4">
+          <a href="https://telegram.me/HavoqandJamoa_Bot" target="_blank" className="no-underline text-secondary">{locale == "uz" ? "Jamoamizga qo’shiling" : "Присоединяйтесь к нашей команде"}</a>
+        </li>
     </ul>
   )
 }
