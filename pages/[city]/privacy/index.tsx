74b6dc1d3cad73e82c165082113f3a5882939e54
shahaa <projects.salim95@gmail.com>
2021-12-08 13:29:25 +0500

    privacy page added

---
+++ pages/[city]/privacy/index.tsx
@@ -0,0 +1,51 @@
+import { Layout } from '@components/common'
+import commerce from '@lib/api/commerce'
+import { GetServerSidePropsContext } from 'next'
+import useTranslation from 'next-translate/useTranslation'
+
+export async function getServerSideProps({
+  preview,
+  locale,
+  locales,
+  query,
+}: GetServerSidePropsContext) {
+  const config = { locale, locales, queryParams: query }
+  const siteInfoPromise = commerce.getSiteInfo({ config, preview })
+  const {
+    categories,
+    brands,
+    topMenu,
+    footerInfoMenu,
+    socials,
+    cities,
+    currentCity,
+  } = await siteInfoPromise
+  if (!currentCity) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      categories,
+      brands,
+      topMenu,
+      currentCity,
+      footerInfoMenu,
+      socials,
+      cities,
+    },
+  }
+}
+
+export default function AboutPage() {
+  const { t: tr } = useTranslation('common')
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: tr('privacy_text') }} />
+    </>
+  )
+}
+
+AboutPage.Layout = Layout
