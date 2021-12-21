f29e5ed1afc18850f4813a86e36321786ab23074
Davron <yul.davron.93@gmail.com>
2021-12-21 15:14:13 +0500

    google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,12 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body className="loading">
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSJ79WZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
