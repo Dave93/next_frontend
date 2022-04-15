38d4ae48cb0bf099bf4ff04203114b1a66911335
Davron Yuldashev <yul.davron.93@gmail.com>
2022-04-15 09:41:24 +0500

    adding bitrix24 analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,16 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           ></noscript>
           <Main />
           <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,u){
+                  var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                  var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+                })(window,document,'https://crm.choparpizza.uz/upload/crm/tag/call.tracker.js');
+              `,
+            }}
+          ></script>
         </body>
       </Html>
     )
