449a1819f02db967d8f094bd327262ec548b5301
Shahzod <shahzod.salim95@gmail.com>
2024-03-08 09:35:06 +0500

    weight

--- components_new/common/SmallCart.tsx
+++ components_new/common/SmallCart.tsx
@@ -417,7 +417,7 @@ const SmallCart: FC<SmallCartProps> = ({ channelName }) => {
                           </div>
                         )}
                         <div className="font-bold text-sm flex-grow mx-1 uppercase">
-                          {lineItem.child && lineItem.child.length > 1
+                          {lineItem.child || lineItem.child.length > 1
                             ? `${
                                 lineItem?.variant?.product?.attribute_data
                                   ?.name[channelName][locale || 'ru']
